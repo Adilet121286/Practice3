8092220c52fc9352cc6f26fc4434c62bf93d8511
Adilet Attokurov <64566842+Adilet121286@users.noreply.github.com>
2020-04-30 17:17:41 -0400

    Update HappyB.java

--- IdeaProjects/Practice3/src/day_1/HappyB.java
+++ IdeaProjects/Practice3/src/day_1/HappyB.java
@@ -3,5 +3,7 @@ package day_1;
 public class HappyB {
     public static void main(String[] args) {
         System.out.println("Hapyy Birthday");
+        System.out.println("Changes");
+        
     }
 }
